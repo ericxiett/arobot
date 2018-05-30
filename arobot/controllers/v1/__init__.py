810aa9d0c43ba9fe1f73f6e5cf9ffca531e803cf
jshen28 <yshxxsjt715@gmail.com>
2018-05-30 11:06:12 +0800

    add raid configuration related interfaces & database apis

--- arobot/controllers/v1/__init__.py
+++ arobot/controllers/v1/__init__.py
@@ -1,11 +1,12 @@
 from pecan import rest, expose
 
-from arobot.controllers.v1 import ipmi
+from arobot.controllers.v1 import ipmi, raid
 
 
 class Controller(rest.RestController):
 
     ipmi_conf = ipmi.IPMIConfController()
+    raid_conf = raid.RAIDConfController()
 
     @expose()
     def index(self):
